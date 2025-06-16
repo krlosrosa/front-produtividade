b4ab9a30d080847c72138b25287df89f068bbf7a
carlos <robertorsilva.krlos@gmail.com>
2025-06-16 10:29:51 -0300

    adjust parseCadastroRapido

--- src/utils/cadastrorapido.ts
+++ src/utils/cadastrorapido.ts
@@ -1,6 +1,33 @@
-import { ProdutividadeFormType, ProdutividadeItemType } from "@/presentation/dashboard/types/produtividade";
+import {
+  ProdutividadeFormType,
+  ProdutividadeItemType,
+} from "@/presentation/dashboard/types/produtividade";
 
 export function parseCadastroRapido(input: string): ProdutividadeItemType {
+  if (input.includes("ç")) {
+    console.log(input)
+    const [
+      transporte = "",
+      idPallet = "",
+      quantidadeCaixa = "",
+      quantidadeUnidade = "",
+      linhasPickingVisitadas = "",
+      segmento = "",
+      empresa = "",
+      processo = "",
+    ] = input.split("ç").map((s) => s.trim());
+
+    return {
+      empresa,
+      transporte,
+      idPallet,
+      quantidadeCaixa: parseInt(quantidadeCaixa),
+      quantidadeUnidade: parseInt(quantidadeUnidade),
+      linhasPickingVisitadas: parseInt(linhasPickingVisitadas),
+      processo: processo,
+      segmento: segmento as "SECO" | "REFRIGERADO",
+    };
+  }
   const [
     transporte = "",
     idPallet = "",
